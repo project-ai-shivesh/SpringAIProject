bc0fb1fd98dc37a7524b8c49c8d8f6f14c26da33
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-05-23 15:41:27 -0500

    Spring MCP Init

---
+++ shopping-cart-mcp/src/main/java/com/java/shivesh/shopping_cart_mcp/ShoppingCartMcpApplication.java
@@ -0,0 +1,13 @@
+package com.java.shivesh.shopping_cart_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartMcpApplication.class, args);
+	}
+
+}
---
+++ shopping-cart-mcp/src/test/java/com/java/shivesh/shopping_cart_mcp/ShoppingCartMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.shivesh.shopping_cart_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
